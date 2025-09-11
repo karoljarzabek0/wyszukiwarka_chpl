c9d51bd9df9631f716116660e2fe4107e408a2d3
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2025-09-11 18:14:33 +0200

    Update create_table.sql

--- db/create_table.sql
+++ db/create_table.sql
@@ -5,10 +5,11 @@ CREATE TABLE leki (
     moc VARCHAR(100),
     kod_atc VARCHAR(20),
     forma VARCHAR(100),
-    producent VARCHAR(255),
     rodzaj VARCHAR(50), -- ludzki, zwierzęcy
     nazwa_postaci_farmaceutycznej VARCHAR(255),
     link_do_ulotki VARCHAR(500),
     link_do_chpl VARCHAR(500),
-    droga_podania VARCHAR(100)
+    droga_podania VARCHAR(100),
+    podmiot_odpowiedzialny VARCHAR(255),
+    podmiot_odpowiedzialny_kraj VARCHAR(100)
 );
