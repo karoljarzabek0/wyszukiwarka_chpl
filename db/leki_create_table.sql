276919592c16e9b16485f021f4849a23b02b432f
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2025-09-17 18:28:55 +0200

    uspójnienie modelu danych z plikiem źródowym

--- db/leki_create_table.sql
+++ db/leki_create_table.sql
@@ -1,15 +1,20 @@
 CREATE TABLE leki (
-    id SERIAL PRIMARY KEY,
-    nazwa_leku VARCHAR(255) NOT NULL,
-    nazwa_lacinska VARCHAR(255),
-    moc VARCHAR(100),
-    kod_atc VARCHAR(20),
-    forma VARCHAR(100),
-    rodzaj VARCHAR(50), -- ludzki, zwierzęcy
+    id_produktu SERIAL PRIMARY KEY,
+    nazwa_produktu VARCHAR(255) NOT NULL,
+    rodzaj_preparatu VARCHAR(255), -- ludzki, zwierzęcy
+    nazwa_powszechna VARCHAR(255),
+    moc VARCHAR(255),
     nazwa_postaci_farmaceutycznej VARCHAR(255),
-    link_do_ulotki VARCHAR(500),
-    link_do_chpl VARCHAR(500),
-    droga_podania VARCHAR(100),
-    podmiot_odpowiedzialny VARCHAR(255),
-    podmiot_odpowiedzialny_kraj VARCHAR(100)
+    podmiot_odpowiedzialny VARCHAR(255), 
+    nazwa_postaci_farmaceutycznej VARCHAR(255),
+    typ_procedury VARCHAR(255),
+    waznosc_pozwolenia VARCHAR(255),
+    podstawa_prawna VARCHAR(255),
+    zakaz_stosowania_zwierzeta VARCHAR(255),
+    ulotka VARCHAR(255),
+    charakterystyka VARCHAR(255),
+    kod_ATC VARCHAR(50),
+    droga_podania VARCHAR(255),
+    nazwa_wytwórcy_importera VARCHAR(255),
+    kraj_wytwórcy_importera VARCHAR(100)
 );
--- db/substancje_create_table.sql
+++ db/substancje_create_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS substancje
 (
-    id_leku integer NOT NULL,
+    id_produktu integer NOT NULL,
     nazwa_substancji VARCHAR(255),
-    ilosc VARCHAR(255),
-    jednostka VARCHAR(50),
-    wartosc VARCHAR(255),
-    jednostka_miary VARCHAR(50),
-    dodatkowe_info TEXT
+    ilosc_substancji VARCHAR(100),
+    jednostka_miary_ilosci_substancji VARCHAR(100),
+    ilosc_preparatu VARCHAR(100),
+    jednostka_miar_ilosci_preparatu VARCHAR(50),
+    informacje_dodatkowe TEXT
 )
