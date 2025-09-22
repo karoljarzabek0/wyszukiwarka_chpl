1fe072d74891d25bab6eb889d0e41648f78c020f
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2025-09-22 11:54:31 +0200

    added atc_codes decoder

--- db/embeddings/vector_search.py
+++ db/embeddings/vector_search.py
@@ -113,7 +113,7 @@ def search_similar(cur, query_text, top_k=5):
 
     return cur.fetchall()
 
-results = search_similar(cur, "ekonomia heterod oksyjna", 50)
+results = search_similar(cur, "refluks", 50)
 df = pd.DataFrame(results, columns=['id_produktu', 'nazwa_produktu', 'kod_atc', 'nazwa_powszechna', 'fts_rank', 'vector_rank', 'combined_rank', 'substancje'])
 print(df)
 # for row in results:
