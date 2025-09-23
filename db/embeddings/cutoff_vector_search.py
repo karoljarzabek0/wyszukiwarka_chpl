6ce8504655d1d5b9b7a18ade4e85ab0daf4125de
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2025-09-23 13:06:16 +0200

    debugged too strict cutoff search

--- db/embeddings/cutoff_vector_search.py
+++ db/embeddings/cutoff_vector_search.py
@@ -73,25 +73,31 @@ fts_filter AS (
     v.id_produktu,
     v.id_fragmentu,
     v.vector_rank,
-    ts_rank(t.fts_vector, q.query_text) AS fts_rank
+    ts_rank(t.fts_vector, q.query_text, 32) AS fts_rank
     FROM vector_search v
     LEFT JOIN tresc_chpl t ON v.id_produktu = t.id_produktu
     CROSS JOIN query_cte q
-    WHERE t.fts_vector @@ q.query_text AND ts_rank(t.fts_vector, q.query_text) > 0.05
+    WHERE t.fts_vector @@ q.query_text AND ts_rank(t.fts_vector, q.query_text, 32) >= 0.05
 )
 SELECT l.nazwa_produktu, l.kod_atc, ROUND(f.vector_rank::numeric, 4)::float, ROUND(f.fts_rank::numeric, 4)::float
+,ts_headline('polishv2', fr.tresc_fragmentu, q.query_text,'MaxFragments=2, MaxWords=10, MinWords=5, StartSel=<<, StopSel=>>')--, fr.tresc_fragmentu
 FROM fts_filter f
+CROSS JOIN query_cte q
 LEFT JOIN leki l ON f.id_produktu = l.id_produktu
+LEFT JOIN tresc_chpl t ON f.id_produktu = t.id_produktu
+LEFT JOIN fragmenty fr ON f.id_fragmentu = fr.id_fragmentu
 ORDER BY f.vector_rank ASC
+LIMIT 30;
         """,
         (query_text, query_embedding.tolist(), query_embedding.tolist())
     )
 
     return cur.fetchall()
 
-results = search_similar(cur, "refluks", 50)
+results = search_similar(cur, "działania OR nieporządane OR paracetamol", 50)
 # df = pd.DataFrame(results, columns=['id_produktu', 'nazwa_produktu', 'kod_atc', 'nazwa_powszechna', 'fts_rank', 'vector_rank', 'combined_rank', 'substancje'])
 # print(df)
+print("nazwa_produktu, kod_atc, vector_rank, fts_rank, snippet")
 for row in results:
     print(row)
 
