362113dd112e77be0073e9f3f9f11e4d27dd05d3
karoljarzabek0 <183915723+karoljarzabek0@users.noreply.github.com>
2025-09-25 13:19:10 +0200

    add ATC codes and update product route to include SVG icons and refund data

--- web_app/routes_fronend.py
+++ web_app/routes_fronend.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from db_helper import get_db_connection
 import os
+import json
 
 frontend_bp = Blueprint("frontend", __name__)
 
@@ -18,14 +19,69 @@ def produkt(slug):
     with open(sql_path, 'r', encoding='utf-8') as f:
         lek_query = f.read()
     cur.execute(lek_query, (slug,))
-    bases = cur.fetchall()
+    bases = cur.fetchone()
 
-    base = { 
+    if bases:
+        base = {
+            "id_produktu": bases[0],
+            "nazwa_produktu": bases[1],
+            "slug": bases[2],
+            "nazwa_powszechna": bases[3],
+            "moc": bases[4],
+            "nazwa_postaci_farmaceutycznej": bases[5],
+            "droga_podania": bases[6],
+            "charakterystyka": bases[7],
+            "podmiot_odpowiedzialny": bases[8],
+            "nazwa_wytwórcy_importera": bases[9],
+            "kraj_wytwórcy_importera": bases[10],
+            "name": bases[11],
+            "atc_name": bases[12],
+            "kod_atc": bases[13]
+        }
+    else:
+        base = {}
 
-    }
-    refundacja = {}
+    sql_path = os.path.join(os.path.dirname(__file__), 'queries', 'refundacja.sql')
+    print(f"Loading SQL from: {sql_path}")
+    with open(sql_path, 'r', encoding='utf-8') as f:
+        refundacja_query = f.read()
+    cur.execute(refundacja_query, (slug,))
+    refundacja_data = cur.fetchall()
+
+    refundacja = []
+    for row in refundacja_data:
+        refundacja.append({
+            "id_produktu": row[0],
+            "nazwa_produktu": row[1],
+            "moc": row[2],
+            "ilosc": row[3],
+            "cena_detaliczna": row[4],
+            "poziom_odplatnosci": row[5],
+            "wysokosc_doplaty": row[6],
+            "zakres_objety_refundacja": row[7]
+        })
+
+    atc_letter = bases['kod_atc'][0:1]
+    icons = {'A': 'stomach.svg',
+             'B': 'blood.svg',
+             'C': 'heart.svg',
+             'D': 'skin.svg',
+             'H': 'hormones.svg',
+             'J': 'virus.svg',
+             'L': 'cancer.svg',
+             'M': 'bones.svg',
+             'N': 'brain.svg',
+             'P': 'bug.svg',
+             'R': 'lungs.svg',
+             'S': 'eye.svg',
+             'V': 'other.svg'
+             }
+    try:
+        svg = icons[atc_letter]
+    except Exception as e:
+        svg = 'other.svg'
 
     cur.close()
     conn.close()
     
-    return render_template('lek.html', base=base, refundacja=refundacja)
+    return render_template('lek.html', base=base, refundacja=refundacja, svg=svg)
